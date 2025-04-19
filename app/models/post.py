046fa93cc3cc5bbeddbb014d6c95383afe35e39e
Ebrahem <ebrahemhashem72@gmail.com>
2025-04-19 21:29:12 +0200

    create post table

---
+++ app/models/post.py
@@ -0,0 +1,15 @@
+import datetime
+from app import db
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
+
+relationship = db.relationship
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime, default=datetime.datetime.now)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    comments = relationship('Comment', backref='post', lazy=True)
